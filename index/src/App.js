6644f182e13c12169baf92a1fac6f2922b5c0c37
Peter Daayeng <peterdaayeng@gmail.com>
2023-01-09 00:12:57 +0000

    changes made successfully!

--- index/src/App.js
+++ index/src/App.js
@@ -1,8 +1,5 @@
 import logo from './logo.svg';
 import './App.css';
-import Fetch from './Component/Fetch';
-import Likebutton from './Component/Likebutton';
-import InstaLike from './Component/InstaLike';
 import Crud from './Component/Crud';
 import Index from './Component/Index';
 import Test from './Component/Test';
--- index/src/Component/Index.js
+++ index/src/Component/Index.js
@@ -2,84 +2,57 @@ import { React, useState, useEffect } from "react";
 import Axios from "axios";
 import axios from "axios";
 function Index() {
-  const [deleteId,setdeleteId]= useState(1)
-  const [idFromdelete,setdelete] = useState(1)
-  const [idFrombtnClick,setIdFrombtnClick] = useState(1)
+  const [deleteId, setdeleteId] = useState(1);
+  const [idFromdelete, setdelete] = useState(1);
+  const [idFrombtnClick, setIdFrombtnClick] = useState(1);
   const [idnumb, setid] = useState(1);
   const [data, setdata] = useState({
     firstname: "",
     lastname: "",
     id: "",
   });
-  function delete_user(e){
-    e.preventDefault();
-    setdelete(deleteId)
-    axios.delete(`http://127.0.0.1:8000/remove/${idFromdelete}`)
-    .then(response=>{
-      console.log(response)
-
-    })
-    .catch(
-      error=>{
-        console.log(error)
-      }
-    )
-    
+  function Onload (){
+    const [loading,setloading] = useEffect(false)
+    useEffect(()=>{
+      setloading(true);
+      setTimeout(() => {
+        setloading(false)
+      }, 2000);
+    }, [])
   }
-function handleSearch (){
-  setIdFrombtnClick(idnumb)
-  //
-}
-function submit(e){
-  e.preventDefault();
-  axios.post('http://127.0.0.1:8000/new/',data)
-  .then(
-    res=>{
-      console.log(res)
-      
-    }
-  )
-  .catch(
-    err=>{
-console.log(err)
-    }
-  )
-}
-
-  // function submit(e) {
-  //   e.preventDefault();
-  //   fetch("http://127.0.0.1:8000/new/",{
-  //     method:'POST',
-  //     mode:"no-cors",
-  //     headers:{
-  //       "Content-Type":"application/json",
-
-  //     }
-  //     ,
-  //     credentials:'same-origin',
-  //     body:JSON.stringify(data)
-  //   })
-
-  //     .then((response) => {
-  //       return response.json()
-  //     })
-      
-  //     .then((json)=>{
-  //       console.log(json) 
-  //     }
-        
-       
-  //     )
-  //     .catch((err) => {
-  //       console.log(err);
-  //     });
-  // }
+  function delete_user(e) {
+    e.preventDefault();
+    setdelete(deleteId);
+    axios
+      .delete(`http://127.0.0.1:8000/remove/${idFromdelete}`)
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+  function handleSearch() {
+    setIdFrombtnClick(idnumb);
+    //
+  }
+  function submit(e) {
+    e.preventDefault();
+    axios
+      .post("http://127.0.0.1:8000/new/", data)
+      .then(() => {
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  } 
 
   useEffect(() => {
     Axios.get(`http://127.0.0.1:8000/user/${idFrombtnClick}`)
       .then((response) => {
         // setpots(response.data);
         console.log(response);
+        // Onload()
       })
       .catch((err) => {
         console.log(err);
@@ -114,27 +87,35 @@ console.log(err)
           onChange={(e) => setdata({ ...data, id: e.target.value })}
           placeholder="id"
         />
-        <br/>
+        <br />
         <button>submit</button>
         <br />
       </form>
       <hr />
-      
+      <div className="container"> loading ? 
+<div className="loader-container">
+  <div className="spinner">
+    <div className="object"></div>
+  <div className="main">
+    
+    </div>
+  </div>
+</div>
+
+</div> 
       <input
         placeholder="search user"
         value={idnumb}
-        onChange={e=>setid(e.target.value)}
+        onChange={(e) => setid(e.target.value)}
       ></input>
-      <button onClick={handleSearch}>search</button>
-<hr/>
+      <button onClick={handleSearch} className='button'>search</button>
+      <hr />
       <input
         placeholder="delete user"
         value={deleteId}
-        onChange={e=>setdeleteId(e.target.value)}
+        onChange={(e) => setdeleteId(e.target.value)}
       ></input>
       <button onClick={delete_user}> Delete</button>
-
-    
     </div>
   );
 }
